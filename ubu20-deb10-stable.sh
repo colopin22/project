03b184e56778f3531d65730e78704899e594da23
colopin22 <155756271+colopin22@users.noreply.github.com>
2024-04-10 03:35:44 +0700

    Add files via upload

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -512,7 +512,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit IP & Quota"
-wget https://raw.githubusercontent.com/gemilangvip/autoscript-vvip/main/udpcustom/udp-custom.sh && chmod +x udp-custom.sh && ./udp-custom.sh
+wget -q https://raw.githubusercontent.com/colopin22/project/main/config/udp-mini && chmod +x udp-mini && ./udp-mini
 
 # // Installing UDP Mini
 mkdir -p /usr/local/kyt/
@@ -681,6 +681,28 @@ if [ -d '/usr/local/ddos' ]; then
 else
 	mkdir /usr/local/ddos
 fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+echo
+cat /usr/local/ddos/LICENSE | less
 
 clear
 # banner
