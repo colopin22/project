2a972a8ca4bbd3762acf9ccc53a6b07eb4f82348
colopin22 <155756271+colopin22@users.noreply.github.com>
2024-04-10 13:06:19 +0700

    Update ubu20-deb10-stable.sh

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -521,6 +521,18 @@ chmod +x /usr/local/kyt/udp-mini
 wget -q -O /etc/systemd/system/udp-mini-1.service "${REPO}files/udp-mini-1.service"
 wget -q -O /etc/systemd/system/udp-mini-2.service "${REPO}files/udp-mini-2.service"
 wget -q -O /etc/systemd/system/udp-mini-3.service "${REPO}files/udp-mini-3.service"
+systemctl disable udp-mini-1
+systemctl stop udp-mini-1
+systemctl enable udp-mini-1
+systemctl start udp-mini-1
+systemctl disable udp-mini-2
+systemctl stop udp-mini-2
+systemctl enable udp-mini-2
+systemctl start udp-mini-2
+systemctl disable udp-mini-3
+systemctl stop udp-mini-3
+systemctl enable udp-mini-3
+systemctl start udp-mini-3
 print_success "Limit IP Service"
 }
 
